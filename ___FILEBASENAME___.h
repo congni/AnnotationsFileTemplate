a9bf161a6de427381e9be70bd12cadc4805830c3
congni <983818495@qq.com>
2016-10-26 17:20:30 +0800

    模板文件
    模板文件

---
+++ ___FILEBASENAME___.h
@@ -0,0 +1,15 @@
+//
+//  @fileName  ___FILENAME___
+//  @abstract  对于此文件功能性描述，方便别人知道此文件主要实现的功能
+//  @author    ___FULLUSERNAME___ 创建于 ___DATE___.
+//  @revise    ___FULLUSERNAME___ 最后修改于 ___DATE___.
+//  @version   当前版本号 1.0(___DATE___).
+//___COPYRIGHT___
+//
+
+___IMPORTHEADER_cocoaTouchSubclass___
+
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : ___VARIABLE_cocoaTouchSubclass___
+
+@end
---
+++ ___FILEBASENAME___.m
@@ -0,0 +1,14 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  ___FULLUSERNAME___ 创建于 ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+@end
